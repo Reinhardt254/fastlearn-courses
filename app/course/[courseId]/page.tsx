193706eafb1b5c00fbb68108aec3c0ed5b7faee9
Reinhardt Lagat <56647015+Reinhardt254@users.noreply.github.com>
2024-02-06 20:56:20 +0300

    my first commit

--- app/course/[courseId]/page.tsx
+++ app/course/[courseId]/page.tsx
@@ -14,7 +14,7 @@ interface ProductPageProps {
    },
 }
 
-const Course = ({ params }: ProductPageProps) => {
+const ProductCourses = ({ params }: ProductPageProps) => {
   const router = useRouter()
   const items = data;
   const products = items.filter((item) => item.id == params.courseId)
@@ -114,4 +114,4 @@ const onBuyNow = (item: Product) => {
   )
 }
 
-export default Course
+export default ProductCourses
