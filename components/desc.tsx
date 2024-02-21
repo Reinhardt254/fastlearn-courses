bf27a2dbfb48260877bec7ae74f44c2eaa9942b0
Reinhardt Lagat <56647015+Reinhardt254@users.noreply.github.com>
2024-02-21 17:48:04 +0300

    fixed image bg colour

--- components/desc.tsx
+++ components/desc.tsx
@@ -5,7 +5,7 @@ const Desc = () => {
   return (
     <div className='pt-10 bg-white mt-2 border-t-2 pb-10 md:pt-16 md:pb-20'>
       <div className='w-full flex-col md:mx-10 md:flex-row flex'>
-         <div className='bg-blue-500 md:w-1/2 flex h-full justify-center items-center md:rounded-tl-full md:rounded-br-full w-full'>
+         <div className='md:bg-blue-500 md:w-1/2 flex h-full justify-center items-center md:rounded-tl-full md:rounded-br-full w-full bg-white'>
             <Image 
               src="/logoimage.png"
               height={400}
@@ -17,8 +17,8 @@ const Desc = () => {
          <div className='md:w-1/2 w-full'>
             <div className='flex justify-center items-center w-full h-full md:pl-16'>
              <div className='flex justify-cente items-cente h-full w-full flex-col'>
-             <div className='bg-white rounded-lg p-5 space-y-2'>
-               <h1 className='text-blue-500 font- text-5xl'>How it works</h1> 
+             <div className='bg-white rounded-lg p-5 space-y-2 md:border-t-0 border-t-2 border-gray-300'>
+               <h1 className='text-blue-500 font- text-4xl'>How it works</h1> 
                <p className='text-gray-700 text-2xl'> &#x2022; Select Course</p>
                <p className='text-gray-700 text-2xl'> &#x2022; Add to cart or buy now</p>
                <p className='text-gray-700 text-2xl'>&#x2022; Proceed to Checkout</p>
